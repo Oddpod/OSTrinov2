9dc7b3aa34d0440f34ab1f12c7f0a1e1f36e5a19
Odd Harald Sjursen Sande <oddh123@gmail.com>
2018-04-06 23:45:35 +0200

    Add ProgressNotification class

---
+++ app/src/main/java/com/odd/ostrinov2/tools/ProgressNotification.kt
@@ -0,0 +1,87 @@
+package com.odd.ostrinov2.tools
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.TaskStackBuilder
+import android.support.v7.app.NotificationCompat
+import com.odd.ostrinov2.MainActivity
+import com.odd.ostrinov2.R
+
+class ProgressNotification(private val mTitle: String,
+                           private val mContext: Context, private val progressText: String) {
+
+    private var mBuilder: NotificationCompat.Builder
+    private val mId: Int = 0
+    private var mNotifyManager: NotificationManager? = null
+
+    init {
+        mNotifyManager = mContext.getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        mBuilder = NotificationCompat.Builder(mContext)
+        setStartedNotification()
+    }
+
+    fun setStartedNotification() {
+        mBuilder.setSmallIcon(R.drawable.ic_stat_name).setContentTitle(mTitle)
+                .setContentText("Started")
+
+        val resultIntent = Intent(mContext, MainActivity::class.java)
+
+        // The stack builder object will contain an artificial back stack for
+        // the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        val stackBuilder = TaskStackBuilder.create(mContext)
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity::class.java)
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent)
+        val resultPendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        mBuilder.setContentIntent(resultPendingIntent)
+
+        mNotifyManager!!.notify(mId, mBuilder.build())
+    }
+
+    fun updateProgress(progress: Int?, totalItems: Int) {
+        if (progress == 1)
+            setProgressNotification()
+        updateProgressNotification(progress!!, totalItems)
+    }
+
+    private fun setProgressNotification() {
+        mBuilder.setContentTitle(mTitle).setContentText(progressText)
+        mNotifyManager!!.notify(mId, mBuilder.build())
+    }
+
+    private fun updateProgressNotification(incr: Int, totalItems: Int) {
+        mBuilder.setProgress(totalItems, incr, false)
+        mNotifyManager!!.notify(mId, mBuilder.build())
+    }
+
+    fun setCompletedNotification() {
+        mBuilder.setSmallIcon(R.drawable.ic_stat_name).setContentTitle(mTitle)
+                .setContentText("Completed")
+
+        val resultIntent = Intent(mContext, MainActivity::class.java)
+
+        // The stack builder object will contain an artificial back stack for
+        // the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        val stackBuilder = TaskStackBuilder.create(mContext)
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity::class.java)
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent)
+        val resultPendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        mBuilder.setContentIntent(resultPendingIntent)
+
+        mNotifyManager!!.notify(mId, mBuilder.build())
+    }
+}
