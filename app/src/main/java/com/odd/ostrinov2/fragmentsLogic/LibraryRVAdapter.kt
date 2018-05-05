7753220df83b3c33b538c23a760bb91e2c09da5a
Odd Harald Sjursen Sande <oddh123@gmail.com>
2018-05-05 14:25:22 +0200

    Delete LibraryRVAdapter

--- app/src/main/java/com/odd/ostrinov2/fragmentsLogic/LibraryRVAdapter.kt
+++
@@ -1,6 +0,0 @@
-package com.odd.ostrinov2.fragmentsLogic
-
-import android.content.Context
-import com.odd.ostrinov2.Ost
-
-class LibraryRVAdapter(private val mContext: Context, ostListin: List<Ost>) : PlaylistRVAdapter(mContext, ostListin)
