ea767d6ee3dd1a7a5798f54cf00d43fc7084206d
Odd Harald Sjursen Sande <oddh123@gmail.com>
2018-05-13 23:40:15 +0200

    add callback and launchalertdialog method

--- app/src/main/java/com/odd/ostrinov2/tools/PermissionHandler.kt
+++ app/src/main/java/com/odd/ostrinov2/tools/PermissionHandler.kt
@@ -10,6 +10,7 @@ import android.os.Build
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import com.odd.ostrinov2.Constants
 import com.odd.ostrinov2.MainActivity
 
@@ -17,20 +18,33 @@ fun requestSystemAlertPermission(context: Activity?, requestCode: Int) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
             return
         val packageName = context!!.packageName
-        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + packageName))
+    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
         context.startActivityForResult(intent, requestCode)
     }
 
-    fun isSystemAlertPermissionGranted(context: Context): Boolean {
-        val result = Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(context)
-        return result
-    }
+fun isSystemAlertPermissionGranted(context: Context): Boolean =
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(context)
 
-fun checkPermission(mainActivity: MainActivity) {
-        if (ContextCompat.checkSelfPermission(mainActivity,
-            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(mainActivity,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+fun launchOverlayPermissionNotGrantedDialog(mainActivity: MainActivity) {
+    val builder = AlertDialog.Builder(mainActivity)
+    builder.setMessage("Ooops, this app needs permission to draw over other apps in " +
+            "order to play youtube videos")
+            .setCancelable(false)
+            .setNegativeButton("I don't want to play anything", null)
+            .setPositiveButton("Give permission") { _, _ ->
+                requestSystemAlertPermission(
+                        mainActivity, 3)
+            }
+    val alert = builder.create()
+    alert.show()
+
+}
+
+fun checkPermission(mainActivity: MainActivity, callback: Runnable) {
+    if (ContextCompat.checkSelfPermission(mainActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(mainActivity,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
         // Should we show an explanation?
         if (ActivityCompat.shouldShowRequestPermissionRationale(mainActivity,
@@ -51,5 +65,8 @@ fun checkPermission(mainActivity: MainActivity) {
             // app-defined int constant. The callback method gets the
             // result of the request.
         }
+    } else {
+        callback.run()
     }
+
 }
