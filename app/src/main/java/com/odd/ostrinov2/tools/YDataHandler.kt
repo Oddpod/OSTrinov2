ae8a72453c4e034a600785d9e12bce1df3182c91
Odd Harald Sjursen Sande <oddh123@gmail.com>
2018-05-20 20:48:28 +0200

    Add getVideoTitle method

--- app/src/main/java/com/odd/ostrinov2/tools/YDataHandler.kt
+++ app/src/main/java/com/odd/ostrinov2/tools/YDataHandler.kt
@@ -1,6 +1,7 @@
 package com.odd.ostrinov2.tools
 
 import com.odd.ostrinov2.Constants
+import org.json.JSONArray
 import org.json.JSONObject
 
 internal object YDataHandler {
@@ -14,5 +15,20 @@ internal object YDataHandler {
         val jsonStr = sh.makeServiceCall(jsonUrl)
         val pageInfo = JSONObject(jsonStr).getJSONObject("pageInfo")
         return pageInfo.getInt("totalResults")
+    }
+
+    fun getVideoTitle(videoId: String): String {
+        val sh = HttpHandler()
+        val jsonUrl = """https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet
+            |&id=$videoId&key=${Constants.YDATA_API_TOKEN}""".trimMargin()
+
+        val jsonStr = sh.makeServiceCall(jsonUrl)
+        val jsonObj = JSONObject(jsonStr)
+        val items: JSONArray = jsonObj.getJSONArray("items")
+        if (items.length() == 0) {
+            return ""
+        }
+        val snippet = items.getJSONObject(0).getJSONObject("snippet")
+        return snippet.getString("title")
     }
 }
