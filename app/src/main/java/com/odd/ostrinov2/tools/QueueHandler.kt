124490610168635406432930251cec6e9f4a9fb7
Odd Harald Sjursen Sande <oddh123@gmail.com>
2018-06-05 23:04:36 +0200

    Add seperate  Arraydeque for user queued osts
    Replaced queueaddpos

--- app/src/main/java/com/odd/ostrinov2/tools/QueueHandler.kt
+++ app/src/main/java/com/odd/ostrinov2/tools/QueueHandler.kt
@@ -6,18 +6,28 @@ import com.odd.ostrinov2.listeners.PlayerListener
 import java.util.*
 
 
-data class QueueHandler(var ostList: MutableList<Ost>, var startIndex : Int, var shuffle
+class QueueHandler(var ostList: MutableList<Ost>, startIndex : Int, var shuffle
 : Boolean, var playerListener: PlayerListener, private var queueAdapter: QueueAdapter ){
 
-    var queue: Stack<Ost>
-    private var played: Stack<Ost>
+
+
+    private var userQueue = ArrayDeque<Ost>()
+    private var queue: Stack<Ost> = Stack()
+    private var played: Stack<Ost> = Stack()
     var currentlyPlaying : Ost
     private var currPlayingIndex:Int = 0
-    private var queueAddPos: Int = 0
+
+    var queueSize = 0
+        get() = queue.size + userQueue.size
+
+    fun getQueueItem(pos: Int): Ost{
+        return if(pos < userQueue.size)
+            userQueue.elementAt(pos)
+        else
+            queue.elementAt( queue.size - pos - 1)
+    }
 
     init{
-        played = Stack()
-        queue = Stack()
         currentlyPlaying = ostList[startIndex]
         playerListener.updateCurrentlyPlaying(currentlyPlaying.id)
         played.addAll(ostList.subList(0, startIndex))
@@ -27,7 +37,6 @@ data class QueueHandler(var ostList: MutableList<Ost>, var startIndex : Int, var
         if (shuffle) {
             shuffleOn()
         }
-
         queueAdapter.initiateQueue(this)
     }
 
@@ -66,28 +75,37 @@ data class QueueHandler(var ostList: MutableList<Ost>, var startIndex : Int, var
 
     fun shuffleOn() {
         val seed = System.nanoTime()
-        queue.subList(queueAddPos, queue.size).shuffle(Random(seed))
+        queue.addAll(played)
+        played.clear()
+        queue.shuffle(Random(seed))
         shuffle = true
         notifyPlayerListeners()
     }
 
     fun addToQueue(ost: Ost) {
-        queue.add(queue.size - queueAddPos, ost)
-        queueAddPos++
-        ostList.add(currPlayingIndex + 1, ost)
-        currPlayingIndex = ostList.indexOf(currentlyPlaying)
-        notifyPlayerListeners()
+        userQueue.add(ost)
+        notifyPlayerListeners(QueueUpdateAction.ADD, userQueue.size - 1)
     }
 
     fun addToQueue(ostList: List<Ost>) {
         ostList.forEach { addToQueue(it) }
     }
 
-    fun removeFromQueue(ost : Ost) {
-        if(queue.indexOf(ost) <= queueAddPos && queueAddPos!= 0){
-            queueAddPos--
+    fun removeFromQueue(index: Int) {
+        println(index)
+        if(index < userQueue.size){
+            val ostIterator = userQueue.descendingIterator()
+            var i = 0
+            for (ost in ostIterator){
+                if(i == index){
+                    ostIterator.remove()
+                    break
+                }
+                i++
+            }
         }
-        queue.remove(ost)
+        else
+            queue.removeAt(index)
     }
 
     fun previous(): String?{
@@ -100,13 +118,12 @@ data class QueueHandler(var ostList: MutableList<Ost>, var startIndex : Int, var
     }
 
     fun next() : String?{
-        if (!queue.isEmpty()) {
+        if(!userQueue.isEmpty()) {
+            played.push(currentlyPlaying)
+            currentlyPlaying = userQueue.removeFirst()
+        } else if (!queue.isEmpty()) {
             played.push(currentlyPlaying)
             currentlyPlaying = queue.pop()
-        }
-        notifyPlayerListeners()
-        if(queueAddPos != 0){
-            queueAddPos--
         }
         return currentlyPlaying.videoId
     }
